1c472da6aacdbee72adafdcb58ad04143cf725fe
Y2K <yeyintkyaw300@gmail.com>
2022-01-08 14:56:31 -0800

    tailwind template complete

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie App</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto flex flex-col md:flex-row  justify-between items-center py-6 px-4">
+            <ul class="flex items-center flex-col md:flex-row">
+                <li>
+                    <a href="#" class="flex items-center">
+                        <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z">
+                            </path>
+                        </svg>
+                        <span class="ml-2">Moviepedia</span>
+                    </a>
+                </li>
+                <li class="md:ml-16 mt-3 md:mt-0">
+                    <a href="#" class="hover:text-gray-300">Movies</a>
+                </li>
+                <li class="md:ml-6 mt-3 md:mt-0">
+                    <a href="#" class="hover:text-gray-300">TV Shows</a>
+                </li>
+                <li class="md:ml-6 mt-3 md:mt-0">
+                    <a href="#" class="hover:text-gray-300">Actors</a>
+                </li>
+
+            </ul>
+            <div class="flex items-center ">
+                <div class="relative mt-3 md:mt-0">
+                    <input type="text" name=""
+                        class="bg-gray-800 rounded-full w-64 px-4 py-1 pl-8 text-sm focus:outline-none focus:shadow-outline"
+                        id="" placeholder="Search...">
+                    <div class="absolute top-0">
+                        <svg class=" w-4 mt-2 ml-2 fill-current text-gray-500" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="ml-4">
+                    <a href="">
+                        <img src="{{ asset('images/superhero.png') }}" alt="avatar" class="rounded-full w-8 h-8">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</body>
+
+</html>
